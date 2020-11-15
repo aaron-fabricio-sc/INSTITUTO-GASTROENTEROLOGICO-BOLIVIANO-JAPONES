f975397f2d9e03755a03388b49aed523c5858676
Aaron Fabricio <aaronfabricio00@gmail.com>
2020-11-15 13:08:20 -0400

    menu realizado

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const d = document;
+const menu = d.getElementById("menu");
+
+d.addEventListener("click", (e) => {
+  if (e.target.matches(".hamburger") || e.target.matches(`.hamburger *`)) {
+    menu.classList.toggle("movimiento");
+  }
+});
